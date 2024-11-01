1836789bf2fbdb786cf702cb75bd7a2580c71126
Javeria Hamid <javeriahamidali@gmail.com>
2024-11-02 00:01:41 +0500

    sports chatbot

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const qna = [];
 async function getResponseFromLLM() {
   try {
+    const inputRef = document.querySelector("#userQuery");
+    const query = inputRef.value;
+    console.log(query);
+
+    const apibody = {
+      contents: [
+        {
+          parts: [
+            {
+              text: "You are an expert Sports consultant, who answers only and only about sports queries and excuses others",
+            },
+            { text: query },
+          ],
+        },
+      ],
+    };
     const res = await fetch(
       "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyCNp6f5EalKjLAa5fMjU4DGLpd8yE7JIfY",
       {
@@ -9,13 +25,11 @@ async function getResponseFromLLM() {
           "Content-Type": "application/json",
         },
 
-        body: JSON.stringify({
-          contents: [{ parts: [{ text: "Explain how AI works" }] }],
-        }),
+        body: JSON.stringify(apibody),
       }
     );
     const data = await res.json();
-    const responseText = data["candidates"][0]["content"]["parts"][0]["text"];
+    const responseText = data["candidates"][0]["content"]["parts"][0].text;
 
     const container = document.querySelector(".llm");
     const newDiv = document.createElement("div");
@@ -27,7 +41,6 @@ async function getResponseFromLLM() {
     console.log(responseText);
   } catch (err) {
     console.log(err);
+    alert("error while generating response");
   }
 }
-getResponseFromLLM();
-
