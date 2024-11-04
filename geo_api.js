0c3cbf987a8cc354b436a3203097c79031fb775b
Javeria Hamid <javeriahamidali@gmail.com>
2024-11-05 00:20:14 +0500

    geo api

---
+++ geo_api.js
@@ -0,0 +1,19 @@
+
+  function getUserGeoLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (location) => {
+                console.log(location.coords.latitude);
+                console.log(location.coords.longitude);
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    } else {
+        alert("Browser location not supported.");
+    }
+}
+
+
+getUserGeoLocation();
