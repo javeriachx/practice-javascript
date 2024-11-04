73875c973cdf3db9252f474f10123ff568dacc40
Javeria Hamid <javeriahamidali@gmail.com>
2024-11-05 00:02:24 +0500

    weather geo api

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-//geo location api
+function getUserGeoLocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+          (location) => {
+              console.log(location.coords.latitude);
+              console.log(location.coords.longitude);
+          },
+          (error) => {
+              console.log(error);
+          }
+      );
+  } else {
+      alert("Browser location not supported.");
+  }
+}
 
-function getUserGeoLocation(){
-    if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((location)=>{
-        console.log(location.coords.latitude);
-        console.log(location.coords.longitude);
-    },
-    (error) =>{
-        console.log(error);
-    }
+async function getUserMedia() {
+  try {
+      const videoRef = document.querySelector("video"); // Select the video element
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+          const stream = await navigator.mediaDevices.getUserMedia({
+              audio: true,
+              video: { width: 500, height: 300 }
+          });
 
-    );
+          videoRef.srcObject = stream;
+          videoRef.onloadedmetadata = () => {
+              videoRef.play(); // Start video playback
+          };
+          console.log("Video stream started");
+      } else {
+          alert("Media devices not supported in this browser.");
+      }
+  } catch (error) {
+      console.log("Error accessing media devices:", error);
+  }
 }
-else{
-    alert("Browser location not supported.")
-}
-}
-getUserGeoLocation();
+
+getUserMedia();
