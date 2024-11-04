5b6250332387a24410b4022457835fa314bade6c
Javeria Hamid <javeriahamidali@gmail.com>
2024-11-04 18:45:40 +0500

    geo location api

--- index.js
+++ index.js
@@ -1,46 +1,19 @@
-const qna = [];
-async function getResponseFromLLM() {
-  try {
-    const inputRef = document.querySelector("#userQuery");
-    const query = inputRef.value;
-    console.log(query);
+//geo location api
 
-    const apibody = {
-      contents: [
-        {
-          parts: [
-            {
-              text: "You are an expert Sports consultant, who answers only and only about sports queries and excuses others",
-            },
-            { text: query },
-          ],
-        },
-      ],
-    };
-    const res = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyCNp6f5EalKjLAa5fMjU4DGLpd8yE7JIfY",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
+function getUserGeoLocation(){
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((location)=>{
+        console.log(location.coords.latitude);
+        console.log(location.coords.longitude);
+    },
+    (error) =>{
+        console.log(error);
+    }
 
-        body: JSON.stringify(apibody),
-      }
     );
-    const data = await res.json();
-    const responseText = data["candidates"][0]["content"]["parts"][0].text;
-
-    const container = document.querySelector(".llm");
-    const newDiv = document.createElement("div");
-    newDiv.innerHTML = `
-   <p> ${responseText}</p>
-    `;
-
-    container.appendChild(newDiv);
-    console.log(responseText);
-  } catch (err) {
-    console.log(err);
-    alert("error while generating response");
-  }
 }
+else{
+    alert("Browser location not supported.")
+}
+}
+getUserGeoLocation();
